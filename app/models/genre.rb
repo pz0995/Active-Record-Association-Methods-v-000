4d5bb13192a56e8f3b3ecb8b719de5f8c3270eb0
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-30 17:09:21 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,22 @@
+require 'pry'
+
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    genres.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    return genres.artists.count
   end
 
-  def all_artist_names
+  def all_artist_names(name)
     # return an array of strings containing every musician's name
+    artists.each do |artst|
+      artst = artists.name.count
   end
+end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.songs.genres.first
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    songs.find_by_sql("SELECT * FROM songs WHERE artist = 'Drake'")
   end
 end
