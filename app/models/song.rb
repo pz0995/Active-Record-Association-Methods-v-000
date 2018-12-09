e67859c800e576101bc9eae859f47498ffc23b5f
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-09 04:35:03 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.songs.genres.first
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+   self.artist = Artist.create(name: "Drake")
   end
 end
